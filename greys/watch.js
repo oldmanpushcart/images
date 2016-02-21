adbd5ecd76a868558dfdb43ce9838691467ffafb
oldmanpushcart <oldmanpushcart@gmail.com>
2016-02-21 11:44:20 +0800

    Update watch.js

--- greys/watch.js
+++ greys/watch.js
@@ -2,7 +2,7 @@ require(['greys'], function (greys) {
    greys.watching({
 
        before: function (o, a) {
-           o.println(a.clazz.name+"."+a.method.name+"()");
+           o.println('call from remote script: '+a.clazz.name+"."+a.method.name+"()");
        },
 
    });
